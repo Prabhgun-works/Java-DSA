07a59ce8450fc75b6f0365e6e5cf159f005c9ea1
Prabgun-works <prabhgun.web@gmail.com>
2026-01-31 13:35:09 +0530

    Initial commit

---
+++ Reversal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class reversal {
+    public static int Intvervse(int n){
+        int rem = 0 ;
+        return(n/10 , rem*10 + n%10);
+    }
+
+public static void main(String[ args]) {
+    int n = 567;
+    // int arr[] = new int[n]; 
+    // for(int i = 0 ;i < n ; i++) {
+    //     arr[i] = sc.nextInt();
+    // }
+    System.out.println(Intvervse(n));
+}
+}
